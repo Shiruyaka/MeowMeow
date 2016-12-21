c2893fec44d891b5410696e74c7d498b786eaf87
sasandra <wedlowska.mieszanka@gmail.com>
2016-12-21 20:36:51 +0100

    pobranie eby edytowa

---
+++ ApplicationMain.py
@@ -0,0 +1,8 @@
+import Tkinter as tk
+import LoginWindow
+
+mw = tk.Tk()
+
+
+app = LoginWindow.LoginWindow(mw)
+app.mainloop()
--- RegisterWindow.py
+++ RegisterWindow.py
@@ -3,7 +3,13 @@ import Tkinter as tk
 
 
 class RegisterWindow(tk.Frame):
-    def __init__(self, master=None):
+    def __init__(self, master):
+
+        master.title('MeowMeow')
+        master.geometry('350x250')
+        master.resizable(0, 0)
+
         tk.Frame.__init__(self, master=master)
+
         self.grid()
-        pass
+        self.createWidgets()
