21612b84f721773280c2da2c52187944162774a1
Shiruyaka <tomasz.adam.skrzypczak@gmail.com>
2016-12-31 12:27:08 +0100

    :3 Meow Meow

--- Client.py
+++ Client.py
@@ -13,14 +13,23 @@ class Client():
 
     def generate_new_pair_key(self):
         random_generator = Random.new().read
-        pair_key = RSA.generate(2048, random_generator)
+        pair_key = RSA.generate(4096, random_generator)
 
         pub_key_val = pair_key.publickey().exportKey(format='PEM')
         pub_key_id = pub_key_val.split('-----')[2].lstrip().rstrip()
         pub_key_id = pub_key_id[-8:].replace('\n', '')
 
         data = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-        self.db.add_key(pub_key_id, self.id, pub_key_val, data)
+       # self.db.add_key(pub_key_id, self.id, pub_key_val, data)
+
+        file = open('priv_key.pem', 'w')
+        file.write(pair_key.exportKey('PEM'))
+
+        file = open('pub_key.pem', 'w')
+        file.write(pair_key.publickey().exportKey('PEM'))
+
+        file.close()
 
 
-
+#a = Client()
+#a.generate_new_pair_key()
--- Utils.py
+++ Utils.py
@@ -1,18 +1,40 @@
-import RegisterWindow
+# -*- coding: utf-8 -*-
 
+from Crypto.PublicKey import RSA
+from Crypto import Random
+from Crypto.Cipher import PKCS1_OAEP
 
 COLOR_OF_WINDOW = '#99CCFF'
 TYPE_OF_ROOM = ['Your rooms', 'Rooms', 'Friends']
+IV = '0' * 256
+
+def login():
+    pass
 
 def get_key_id(key_file):
-    key = key_file.exportkey()
+    key = key_file.exportKey(format = 'PEM')
     pub_key_id = key.split('-----')[2].lstrip().rstrip()
     pub_key_id = pub_key_id[-8:].replace('\n', '')
 
     return pub_key_id
 
 
+def pgp_enc_msg(key_file):
+    en_msg = None
+    id = get_key_id(key_file)
 
-file = open('serv_pub.pem', 'rb')
-RSA.importKey
-get_key_id(file)
+    session_key = Random.new().read(128)
+    rsaencrypt = PKCS1_OAEP.new(key_file)
+
+
+    ciphertext = rsaencrypt.encrypt(session_key)
+
+
+
+
+file = open('pub_key.pem', 'r')
+key = RSA.importKey(file.read())
+pgp_enc_msg(key)
+
+#print key.exportKey(format='PEM')
+#print get_key_id(key)
