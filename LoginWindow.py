54dc1e8adf986eacb192030630471edd2ddbf499
sasandra <wedlowska.mieszanka@gmail.com>
2017-01-13 22:27:38 +0100

    Try if file exist

--- LoginWindow.py
+++ LoginWindow.py
@@ -41,6 +41,7 @@ class LoginWindow(tk.Frame):
     def go_to_user_window(self, event):
 
         self.error_lbl.forget()
+        self.error_keyring_lbl.forget()
 
         try:
             self.server_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -50,43 +51,50 @@ class LoginWindow(tk.Frame):
             exit(0)
 
         ########## sprawdzac czy jest plik z priv keyring
+        try:
+            public_keyring = Keyring.import_keyring('pub')
+            private_keyring = Keyring.import_keyring('priv')
+            server_key = RSA.importKey(Keyring.find_pubkey_in_ring(public_keyring, whose='Server'))
+            user_key = RSA.importKey(private_keyring[0].priv_key)  # powinno jeszcze id zwracac?
+            ##na razie tylko jedna osoba na komputerze
 
-        public_keyring = Keyring.import_keyring('pub')
-        private_keyring = Keyring.import_keyring('priv')
+            passh = sha1(self.password_entry.get()).hexdigest()
+            msg = Utils.make_msg(('LOG', self.login_entry.get(), passh, Utils.get_key_id(user_key.publickey())))
 
-        server_key = RSA.importKey(Keyring.find_pubkey_in_ring(public_keyring, whose='Server'))
-        user_key = RSA.importKey(private_keyring[0].priv_key) #powinno jeszcze id zwracac?
-        ##na razie tylko jedna osoba na komputerze
+            # print server_key.exportKey()
+
+            msg = Utils.pgp_enc_msg(server_key, user_key, msg)
+            msg.ljust(8192, '=')
+
+            self.server_conn.send(msg)
+            respond = self.server_conn.recv(8192)
+            content = Utils.pgp_dec_msg(respond, public_keyring, private_keyring)
+            print content
+
+            if content[1] == 'OK':
+                user_data = list()
+
+                user_data.append(content[2])
+                user_data.append(self.login_entry.get())
+                user_data.append(self.password_entry.get())
+                user_data.append(content[3])
+                user_data.append(content[4])
+                user_data.append(content[5])
+
+                client = Client.Client(user_data, private_keyring, public_keyring, self.server_conn)
+                app = UserWindowWithTree.UserWindowWithTree(client, self.master)
+                self.destroy()
+            else:
+                self.error_lbl.grid()
+
+        except:
+            print("File doesn't find")
+            self.error_keyring_lbl.grid()
 
 
-        passh = sha1(self.password_entry.get()).hexdigest()
-        msg = Utils.make_msg(('LOG', self.login_entry.get(), passh, Utils.get_key_id(user_key.publickey())))
 
-        #print server_key.exportKey()
 
-        msg = Utils.pgp_enc_msg(server_key, user_key, msg)
-        msg.ljust(8192, '=')
 
-        self.server_conn.send(msg)
-        respond = self.server_conn.recv(8192)
-        content = Utils.pgp_dec_msg(respond, public_keyring, private_keyring)
-        print content
-
-        if content[1] == 'OK':
-            user_data = list()
-
-            user_data.append(content[2])
-            user_data.append(self.login_entry.get())
-            user_data.append(self.password_entry.get())
-            user_data.append(content[3])
-            user_data.append(content[4])
-            user_data.append(content[5])
-
-            client = Client.Client(user_data, private_keyring, public_keyring, self.server_conn)
-            app =  UserWindowWithTree.UserWindowWithTree(client, self.master)
-            self.destroy()
-        else:
-            self.error_lbl.grid()
 
     def change_text(self, event):
         self.register_lbl.configure(foreground='red')
@@ -126,6 +134,7 @@ class LoginWindow(tk.Frame):
         self.register_lbl.bind('<Leave>', self.revert_text)
 
         self.error_lbl = tk.Label(master=self.master, text='Invalid username or password', foreground='red')
+        self.error_keyring_lbl = tk.Label(master=self.master, text='You must to sign up', foreground='red')
 
 
 
